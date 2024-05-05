50a34516e47f65beb2e3e1a5f1124f70b565bcea
greyzusht <grayzusht@gmail.com>
2024-05-05 20:00:56 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ RELEASE="$(rpm -E %fedora)"
 
 
 # Remove undesired packages
-rpm-ostree override remove \
-    gnome-tour \
-    htop \
-    gnome-terminal \
-    gnome-terminal-nautilus \
-    toolbox \
-    xorg-x11-drv-intel
+# rpm-ostree override remove \
+#     gnome-tour \
+#     htop \
+#     gnome-terminal \
+#     gnome-terminal-nautilus \
+#     toolbox \
+#     xorg-x11-drv-intel
     
     
 
@@ -24,35 +24,35 @@ rpm-ostree override remove \
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install \
-    gcc \
-    libxcrypt-compat \
-    libimobiledevice \
-    make \
-    mesa-libGLU \
-    gnome-tweaks \
-    unrar \
-    p7zip \
-    p7zip-plugins \
-    aria2 \
-    fastfetch \
-    lm_sensors \
-    sstp-client \
-    NetworkManager-sstp \
-    NetworkManager-sstp-gnome \
-    screen \
-    freetype-freeworld \
-    gnome-console \
-    code \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-compose-plugin \
-    docker-compose \
-    python3-pip \
-    wireguard-tools \
-    celluloid
+# rpm-ostree install \
+#     gcc \
+#     libxcrypt-compat \
+#     libimobiledevice \
+#     make \
+#     mesa-libGLU \
+#     gnome-tweaks \
+#     unrar \
+#     p7zip \
+#     p7zip-plugins \
+#     aria2 \
+#     fastfetch \
+#     lm_sensors \
+#     sstp-client \
+#     NetworkManager-sstp \
+#     NetworkManager-sstp-gnome \
+#     screen \
+#     freetype-freeworld \
+#     gnome-console \
+#     code \
+#     docker-ce \
+#     docker-ce-cli \
+#     containerd.io \
+#     docker-buildx-plugin \
+#     docker-compose-plugin \
+#     docker-compose \
+#     python3-pip \
+#     wireguard-tools \
+#     celluloid
 
  
 
@@ -60,7 +60,7 @@ rpm-ostree install \
 # rpm-ostree install vlc
 
 #### System Settings
-    systemctl enable flatpak-add-flathub-repo.service && \
-    systemctl enable flatpak-replace-fedora-apps.service && \
+    # systemctl enable flatpak-add-flathub-repo.service && \
+    # systemctl enable flatpak-replace-fedora-apps.service && \
     systemctl enable podman.socket
 
